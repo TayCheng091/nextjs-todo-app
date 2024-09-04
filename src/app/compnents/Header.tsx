4cc9add2425d80eeba319267136000f14824146c
Tay <b1233967@gmail.com>
2024-09-05 00:30:18 +0800

    update header style

--- src/app/compnents/Header.tsx
+++ src/app/compnents/Header.tsx
@@ -11,16 +11,19 @@ function Header() {
   }
 
   return (
-    <div className="sticky top-0 p-5 border">
-      <input
-        type="text"
-        placeholder="Search tasks"
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-      />
-      <Button onClick={handleSearch} style="primary">
-        Search
-      </Button>
+    <div className="sticky top-0 flex justify-between items-center p-5 border">
+      <div>Fresh Search Logo</div>
+      <div className="flex gap-x-2">
+        <input
+          type="text"
+          placeholder="Search tasks"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <Button onClick={handleSearch} style="primary">
+          Search
+        </Button>
+      </div>
     </div>
   );
 }
